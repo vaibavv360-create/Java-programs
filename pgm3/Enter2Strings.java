11358f11a7cc8b1fc5997bc3a59e017d2fbc05c4
vaibavv360-create <vaibavv360@gmail.com>
2026-02-12 21:01:41 +0530

    Create Enter2Strings.java
    Instantiate and call String Operations 
    Instantiate and call String Buffer Operations

---
+++ pgm3/Enter2Strings.java
@@ -0,0 +1,24 @@
+0import textops.StringOperations; 
+import textops.StringBufferOperations; 
+ 
+public class Enter2Strings { 
+ 
+    public static void main(String[] args) { 
+ 
+        if (args.length != 2) { 
+            System.out.println("Usage: java Enter2Strings <str1> <str2>"); 
+            return; 
+        } 
+ 
+        String str1 = args[0]; 
+        String str2 = args[1]; 
+ 
+        // Instantiate and call StringOperations 
+        StringOperations stringOps = new StringOperations(str1, str2); 
+        stringOps.exploreStringMethods(); 
+ 
+        // Instantiate and call StringBufferOperations 
+        StringBufferOperations sbOps = new StringBufferOperations(str1); 
+        sbOps.exploreStringBufferMethods(str2); 
+    } 
+}
