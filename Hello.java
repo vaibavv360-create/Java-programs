2d8bf0914b65f76c7ee29798badfdd11ab8258c4
vaibavv360-create <vaibavv360@gmail.com>
2026-02-19 11:09:23 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.scsvmv.javalab;
+
+/*
+ * Simple Hello class
+ * Always wishes Hello World
+ */ 
+public class Hello
+{
+public void wish() {  
+ 
+ System.out.println("Hello World");
+ }
+}
